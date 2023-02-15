edfd65d43ca35c468084e1a65abde4fa7793b005
Nikita Korneich <korneich.nik@gmail.com>
2023-02-15 20:06:37 +0300

    end

---
+++ README.md
@@ -0,0 +1,26 @@
+🥀 [turtlebot3 with YOLOv3](https://docs.google.com/presentation/d/1JYjwY6ZqBugkgnGPtoBkekLW7GFiUyNSOJXIAy_f5Uo/edit?usp=sharing)  🥀
+
+Metapackage for the turtlebot3 competition in the ROS system. The solution uses the YOLOv3 neural network. To run the metapackage, do the following:
+
+1) run git clone from the [official github](https://emanual.robotis.com/docs/en/platform/turtlebot3/overview/) of the competition
+
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_autorace_2020
+git clone https://github.com/amburkoff/referee_console
+
+2) run git clone of this package and install requirements
+
+git clone https://gitlab.com/moksyasha/ros_moryachki.git
+
+pip install -r requirements.txt
+
+3) Execute the commands:
+
+roslaunch turtlebot3_gazebo turtlebot3_autorace_2020.launch
+
+roslaunch autorace_core_moryachki autorace_core.launch
+
+rosrun referee_console mission_autorace_2020_referee.py
+
---
+++ vision/nodes/pytorchyolo/detect_yolo.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+
+from __future__ import division
+
+import os
+import argparse
+import tqdm
+import random
+import numpy as np
+
+from PIL import Image
+
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+
+from pytorchyolo.models import load_model
+from pytorchyolo.utils.utils import load_classes, rescale_boxes, non_max_suppression, print_environment_info
+from pytorchyolo.utils.datasets import ImageFolder
+from pytorchyolo.utils.transforms import Resize, DEFAULT_TRANSFORMS
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.ticker import NullLocator
+
+
+def detect_image(model, image, img_size=320, conf_thres=0.6, nms_thres=0.5):
+    """Inferences one image with model.
+
+    :param model: Model for inference
+    :type model: models.Darknet
+    :param image: Image to inference
+    :type image: nd.array
+    :param img_size: Size of each image dimension for yolo, defaults to 416
+    :type img_size: int, optional
+    :param conf_thres: Object confidence threshold, defaults to 0.5
+    :type conf_thres: float, optional
+    :param nms_thres: IOU threshold for non-maximum suppression, defaults to 0.5
+    :type nms_thres: float, optional
+    :return: Detections on image with each detection in the format: [x1, y1, x2, y2, confidence, class]
+    :rtype: nd.array
+    """
+    model.eval()  # Set model to evaluation mode
+
+    # Configure input
+    input_img = transforms.Compose([
+        DEFAULT_TRANSFORMS,
+        Resize(img_size)])(
+            (image, np.zeros((1, 5))))[0].unsqueeze(0)
+
+    if torch.cuda.is_available():
+        input_img = input_img.to("cuda")
+
+    # Get detections
+    with torch.no_grad():
+        detections = model(input_img)
+        detections = non_max_suppression(detections, conf_thres, nms_thres)
+        detections = rescale_boxes(detections[0], img_size, image.shape[:2])
+    return detections.numpy()
